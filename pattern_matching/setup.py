b6bc126de2a8201f30361180b3d5b71aadd5b35c
Gregory Werbin <gwerbin@users.noreply.github.com>
2019-07-31 23:11:38 -0400

    Add minimal setup.py

---
+++ pattern_matching/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='pattern-matching',
+    author='kingdom5500',
+    version='0.0.2019.07.31',  # "Alpha" versioning scheme, should probably be replaced with something else
+    py_modules=['match']
+)
