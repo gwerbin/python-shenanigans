d2230205f6c93b35f68309288bdb4689dfae29e2
Gregory Werbin <gregory.werbin@chubb.com>
2019-08-01 10:54:30 -0400

    Add thorough testing, coverage not complete and recursion bug not found yet

--- pattern_matching/match.py
+++ pattern_matching/match.py
@@ -1,3 +1,15 @@
+__all__ = [
+    'match',
+    '__matchresult__',
+]
+
+
+# TODO: figure out if Sphinx has something like this, just a placeholder for now
+def doc_private(func):
+    """ Flag a function as 'private' for documentation purposes """
+    return func
+
+
 class match:
     """ Create a pattern-matching function
     
@@ -35,59 +47,85 @@ class match:
         self.patcall = patcall
         self.retcall = retcall
         self.patterns = []
-        self.creating_pattern = None
+        self.creating_pattern = _notcreating
 
     def __getitem__(self, patterns):
+        if self.creating_pattern is not _notcreating:
+            raise ValueError('A pattern is already being created, cannot create another one.')
+
         pattern_has_incorrect_size = (
             (isinstance(patterns, slice) and self.amount != 1)
             or (isinstance(patterns, tuple) and self.amount != len(patterns))
         )
 
         if pattern_has_incorrect_size:
-            raise ValueError("Pattern has incorrect size")
+            raise TypeError("Pattern has incorrect size")
 
         self.creating_pattern = patterns
         return self
 
+    def __call__(self, *values):
+        if self.creating_pattern is _notcreating:
+            # Call the match function for real; look for matches and apply the correct result
+            return self.match_apply(values)
+        else:
+            # This was called after [] -- we now need to add the return value for the pattern
+            self.add_pattern(self.creating_pattern, values)
+            return self
+
     def add_pattern(self, pattern, result):
         """ Add a (pattern, result) pair """
+        if len(result) == 0:
+            raise TypeError('No result given.')
+
         if len(result) == 1:
             # Tuples are for user convenience when len > 1,
             # but when len == 1 assume that we should unwrap
             result = result[0]
-        self.patterns.append((pattern, result))
-        self.creating_pattern = None
 
+        if isinstance(result, tuple) and result[0] is __matchresult__:
+            if len(result) != 2:
+                raise ValueError('__matchresult__ must occur by itself, or in a length-2 tuple.')
+
+        self.patterns.append((pattern, result))
+        self.creating_pattern = _notcreating
+
+    @doc_private
+    def get_match_result(self, values, pattern):
+        if self.patcall and callable(pattern):
+            # returns whatevers, need not be bool (this lets you use __matchresult__)
+            match_result = pattern(*values)
+        else:
+            # returns bools
+            match_result = self.pattern_matches(pattern, values)
+        return match_result
+
+    @doc_private
+    def get_call_result(self, values, result, match_result):
+        if result is __matchresult__:
+            result = match_result
+        elif isinstance(result, tuple) and result[0] is __matchresult__:
+            result = result[1](match_result)
+
+        if self.retcall and callable(result):
+            result = result(*values)
+
+        return result
+
+    @doc_private
     def match_apply(self, values):
         """ Search for a match and apply the corresponding result """
         if len(values) != self.amount:
-            raise TypeError("Invalid amount of values for match")
+            raise TypeError("Invalid amount of values for match.")
 
         for pattern, result in self.patterns:
-            if self.patcall and callable(pattern):
-                is_match = bool(pattern(*values))
-            else:
-                if not isinstance(pattern, tuple):
-                    # so they don't have to write [:,] or [4,], but they still have to write [(1,2),]
-                    pattern = (pattern,)
-                is_match = self.pattern_matches(pattern, values)
-
-            if is_match:
-                if self.retcall and callable(result):
-                    result = result(*values)
-                return result
+            match_result = self.get_match_result(values, pattern)
+            if match_result:
+                return self.get_call_result(values, result, match_result)
 
         raise ValueError("No matching pattern found.")
-    
-    def __call__(self, *values):
-        if self.creating_pattern is not None:
-            # This was called after [] -- we now need to add the return value for the pattern
-            self.add_pattern(self.creating_pattern, values)
-            return self
-        else:
-            # Call the match function for real; look for matches and apply the correct result
-            return self.match_apply(values)
 
+    @doc_private
     @staticmethod
     def check_range_pattern(pattern, value, include_upper=True):
         """ Test a slice/range pattern """
@@ -96,9 +134,9 @@ class match:
 
         if lower is not None and upper is not None:
             if include_upper:
-                return lower <= value < upper
-            else:
                 return lower <= value <= upper
+            else:
+                return lower <= value < upper
 
         if lower is None and upper is not None:
             if include_upper:
@@ -109,24 +147,40 @@ class match:
         if lower is not None and upper is None:
             return value >= lower
 
-    def pattern_matches(self, patterns, values):
+    @doc_private
+    def pattern_matches(self, pattern, values):
         """ Test if a value matches a (non-callable) pattern """
-        for pattern, value in zip(patterns, values):
-            if pattern == slice(None, None, None):
+        if not isinstance(pattern, tuple):
+            # This lets us iterate over a "length 1" pattern where they wrote [:] or [4] instead of [:,] or [4,]
+            # Note that they still have to write [(1,2),] due to ambiguity in the logic
+            # TODO: move this inside match.check_range_pattern()?
+            pattern = (pattern,)
+
+        for term, value in zip(pattern, values):
+            if term == slice(None, None, None):
                 # this is the catch-all pattern, [:]
                 continue
 
-            if isinstance(pattern, slice):
-                if pattern.step is not None:
+            if isinstance(term, slice):
+                if term.step is not None:
                     # ???
-                    raise NotImplementedError('A range pattern with a step value has no meaning (yet)')
+                    raise NotImplementedError('A range pattern with a step value has no meaning (yet).')
 
                 # this is a range pattern, [a:b]
-                if not self.check_range_pattern(pattern, value):
+                if not self.check_range_pattern(term, value):
                     return False
 
-            elif pattern != value:
+            elif term != value:
                 return False
 
-        # fell through a catch-all pattern at the end
         return True
+
+
+class __matchresult__:
+    def __new__(self):
+        raise TypeError('__matchresult__ cannot be insantiated.')
+
+
+class _notcreating:
+    def __new__(self):
+        raise TypeError('__matchresult__ cannot be insantiated.')
--- pattern_matching/test.py
+++ pattern_matching/test.py
@@ -18,23 +18,37 @@
 #    - macropy https://pypi.org/project/macropy3/
 #  Edge cases?
 
-# Things to consider:
-#   - Hypothesis strategy to generate patterns and values?
+# TODO:
+#   - Hypothesis strategy to generate patterns and values
 import pytest
 
-from match import match
+from match import match, __matchresult__
 
+
+## Testing utils
 
 def assert_raises_many(exc, spec):
     for func, args, kwargs in spec:
         with pytest.raises(exc):
             func(*args, **kwargs)
 
+def test_assert_raises_many():
+    def raise_exc(e, msg=''):
+        raise e(msg)
+    assert_raises_many(ValueError, [
+        (raise_exc, (ValueError,), {}),
+        (raise_exc, (ValueError, 'hi'), {}),
+        (raise_exc, (ValueError,), {'msg': 'hi'}),
+    ])
+
+
+## Basic behavioral tests
 
 def test_slicepattern_1():
     """ Test 1-parameter slice-based patterns """
     f = (
         match()
+            [None]  ('invisible')
             [12:52] ('really big, but not too big')
             [1]     ('unified')
             [7:]    ('seven or more')
@@ -43,6 +57,7 @@ def test_slicepattern_1():
             [:]     ('i dunno')
     )
 
+    assert f(None) == 'invisible'
     assert f(100) == 'seven or more'
     assert f(13) == 'really big, but not too big'
     assert f(9) == 'seven or more'
@@ -54,7 +69,7 @@ def test_slicepattern_1():
     assert f(0) == 'too small and creepy'  # ranges are **inclusive**
     assert f(-20) == 'too small and creepy'
 
-    bad_values = ['qt', None, dict, dict()]
+    bad_values = ['qt', dict, dict()]
     assert_raises_many(TypeError, [(f, (val,), {}) for val in bad_values])
 
 
@@ -62,16 +77,31 @@ def test_retcall():
     """ Test the retcall functionality """
     import math
 
-    safelog = (
-        match()
-            [0] (1.0)
-            [1:] (math.log)
-            [:0] (math.nan)
-    )
-
+    safelog = (match()
+        [0] (1.0)
+        [1:] (math.log)
+        [:0] (math.nan))
     assert safelog(0) == 1.0
     assert abs(safelog(2) - math.log(2)) < 1e05
     assert safelog(-1) is math.nan
+
+    weird1 = (match (retcall=False)
+        ['sum'] (sum)
+        ['any'] (any)
+        [:]     (str))
+    assert weird1('sum') is sum
+    assert weird1('any') is any
+    assert weird1(123)   is str
+
+    weird2 = (match ()
+        ['sum'] (sum)
+        ['any'] (any)
+        [:]     (str))
+    with pytest.raises(TypeError, match='unsupported operand type'):
+        # trying to call sum('sum')
+        weird2('sum')
+    assert weird2('any') == True
+    assert weird2(123) == '123'
 
 
 def test_patcall():
@@ -85,6 +115,30 @@ def test_patcall():
     assert friendly_isdigit('hello') == 'Its a string :)'
     assert friendly_isdigit(None) == 'Not a string :('
     assert friendly_isdigit(25) == 'Not a string :('
+
+    weird1 = (match (patcall=False)
+        [sum] ('sum')
+        [any] ('any')
+        [:]   ('other'))
+    assert weird1(sum) == 'sum'
+    assert weird1(any) == 'any'
+    assert weird1(123) == 'other'
+
+    weird2 = (match()
+        [sum] ('sum')
+        [any] ('any')
+        [:]   ('other'))
+    assert weird2([1,2]) == 'sum'
+    assert weird2([-1,1]) == 'any'
+    assert weird2([0,0]) == 'other'
+    assert_raises_many(TypeError, [
+        # trying to call sum(sum)
+        (weird2, (sum,), {}),
+        # trying to call sum(any)
+        (weird2, (any,), {}),
+        # trying to call sum(123)
+        (weird2, (123,), {}),
+    ])
 
 
 def test_init():
@@ -109,9 +163,117 @@ def test_rangepattern_impl():
     assert match.check_range_pattern(slice(None,1), 1, include_upper=False) is False
     with pytest.raises(NotImplementedError):
         # result of check_range_pattern with slice(None,None,None) is "undefined behavior"
-        match().pattern_matches([slice(None,None,3)], [None])
+        match().pattern_matches(slice(None,None,3), [None])
 
 
+
+## Individual internal components
+
+def test_getitem():
+    m = match(2)[:,:]
+    assert m.creating_pattern == (slice(None), slice(None))
+
+    m = match()[:]
+    assert m.creating_pattern == slice(None)
+
+    m = match()[:,]
+    assert m.creating_pattern == (slice(None),)
+
+    # Error if wrong pattern size
+    m = match(2)
+    with pytest.raises(TypeError):
+        m[:]
+    with pytest.raises(TypeError):
+        m[:,:,:]
+
+    # Error if trying to create a pattern while already creating a pattern
+    m = match()[:]
+    with pytest.raises(ValueError):
+        m[:]
+
+    # Error if result is empty (see test_add_pattern)
+    m = match()
+    with pytest.raises(TypeError):
+        m[:]()
+
+
+def test_add_pattern():
+    m = match()
+    m.add_pattern(3, (3,))
+    assert m.patterns[0] == (3, 3)
+
+    # This is confusing and asymmetric, i hate it
+    m = match()
+    m.add_pattern((3,), (3,))
+    assert m.patterns[0] == ((3,), 3)
+    m.add_pattern((slice(None),), (3,))
+    assert m.patterns[1] == ((slice(None),), 3)
+    m.add_pattern(slice(None), (3,))
+    assert m.patterns[2] == (slice(None), 3)
+
+    m = match()
+    m.add_pattern(lambda x: x+1, (__matchresult__,))
+    m.add_pattern(lambda x: x+1, (__matchresult__, lambda x: x/10))
+    # Error if __matchresult__ used incorrectly
+    with pytest.raises(ValueError):
+        m.add_pattern(lambda x: x+1, ((__matchresult__,),))
+    with pytest.raises(ValueError):
+        m.add_pattern(lambda x: x+1, ((__matchresult__, 1, 2),))
+
+    # Error if result is empty
+    m = match()
+    with pytest.raises(TypeError):
+        m.add_pattern(slice(None), ())
+
+
+def test_get_match_result():
+    m = match()
+    # "Exact" matches always return bool
+    assert m.get_match_result((1,), slice(None)) == True
+    assert m.get_match_result((1,), 1) == True
+    # "Called" matches return the result of the call
+    assert m.get_match_result(([-1, 1],), sum) == 0
+
+
+def test_get_call_result():
+    m = match()
+    m.get_call_result(('hello',), __matchresult__, 'this will be returned') == 'this will be returned'
+    m.get_call_result(('hello',), (__matchresult__, len), 'this will not be returned') == len('this will not be returned')
+    m.get_call_result(('hello',), len, 'this will not be returned') == len('hello')
+    m.get_call_result(('hello',), 'this will be returned', 'this can be anything!') == 'this will be returned'
+
+
+def test_match_apply():
+    # TODO
+    pass
+
+
+def test_check_range_pattern():
+    #assert     match.check_range_pattern(slice(None), 3)
+    assert not match.check_range_pattern(slice(5), 10)
+    assert     match.check_range_pattern(slice(5), 5)
+    assert not match.check_range_pattern(slice(5), 5, include_upper=False)
+    assert not match.check_range_pattern(slice(5,None), 1)
+    assert     match.check_range_pattern(slice(0, 5), 3)
+    assert     match.check_range_pattern(slice(0, 5), 0)
+    assert     match.check_range_pattern(slice(0, 5), 5)
+    assert not match.check_range_pattern(slice(0, 5), 5, include_upper=False)
+    assert not match.check_range_pattern(slice(0, 5), 10)
+    assert not match.check_range_pattern(slice(0, 5), -5)
+    assert     match.check_range_pattern(slice('a', 'c'), 'b')
+    assert not match.check_range_pattern(slice('0', '9'), 'b')
+    assert     match.check_range_pattern(slice('0', '9'), '9')
+    assert not match.check_range_pattern(slice('0', '9'), '9', include_upper=False)
+
+
+def test_pattern_matches():
+    # TODO
+    pass
+
+
+## Silly tests for timing and demo of capabilities
+
+@pytest.mark.skip('Recursion error, needs fixing')
 def test_patcall_complicated(benchmark):
     """ Test the patcall functionality by implementing an integer calculator """
     import re
@@ -127,27 +289,31 @@ def test_patcall_complicated(benchmark):
     plus = re.compile(r'\s*\+\s*')
     parens = re.compile(r'\(\s*(.+?)\s*\)')
 
-    #def print_result(f):
-    #    i = 0 
-    #    def _f(*args, **kwargs):
-    #        result = f(*args, **kwargs)
-    #        nonlocal i
-    #        i += 1
-    #        print(i, f.__name__, repr(result), repr(args), repr(kwargs))
-    #        return result
-    #    return _f
+    doprint = False
 
-    #@print_results
+    def print_result(f):
+        i = -1
+        def _f(*args, **kwargs):
+            nonlocal i
+            i += 1
+            print('call', i, f.__name__, '_', repr(args), repr(kwargs)) if doprint else None
+            result = f(*args, **kwargs)
+            print('result', i, f.__name__, repr(result), repr(args), repr(kwargs)) if doprint else None
+            return result
+        return _f
+
+    @print_result
+    def process_parens_match(m):
+        print(m)
+        return str(intaddexpr(m[1]))
+
+    @print_result
     def process_parens(x):
-        return intaddexpr(parens.sub(lambda m: str(intaddexpr(m[1])), x))
+        return intaddexpr(parens.sub(process_parens_match, x))
 
-    #@print_results
+    @print_result
     def process_plus(x):
         return intaddexpr(str(sum(map(intaddexpr, plus.split(x)))))
-
-    #@print_results
-    def process_minus(x):
-        return -intaddexpr(x[1:])
 
     intaddexpr = (
         match()
@@ -156,10 +322,10 @@ def test_patcall_complicated(benchmark):
             #[parens.search]         (lambda x: parens.sub(lambda m: str(intaddexpr(m[0][1:-1])), x))
             #[plus.search]           (lambda x: sum(map(intaddexpr, plus.split(x))))
             #[lambda x: x[0] == '-'] (lambda x: -intaddexpr(x[1:]))
-            [parens.search]         (process_parens)
-            [plus.search]           (process_plus)
-            [lambda x: x[0] == '-'] (process_minus)
-            [:]                     (raises(ValueError, 'Not a valid integer: {}'.format))
+            [process_parens]         (__matchresult__)
+            [process_plus]           (__matchresult__)
+            [lambda x: x[0] == '-']  (lambda x: -intaddexpr(x[1:]))
+            [:]                      (raises(ValueError, 'Not a valid integer: {}'.format))
     )
 
     assert intaddexpr('-12') == -12
@@ -178,6 +344,7 @@ def test_patcall_complicated(benchmark):
     benchmark(intaddexpr, '-( 2 + 1 ) + -3')
 
 
+@pytest.mark.skip('No point without the other test')
 def test_perf_baseline(benchmark):
     """ Equivalent imperative code performance, to see match() overhead """
     import re
